f0f847f41bef76f08acef67556b9283dbac08a02
John T. Foster <johntfosterjr@gmail.com>
2021-02-23 17:37:46 -0600

    Add noxfixes flag

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ rm -f /tmp/.X0-lock
 Xvfb :0 &
 sleep 1
 
-x11vnc -rfbport $VNC_PORT -display :0 -usepw -forever &
+x11vnc -noxfixes -rfbport $VNC_PORT -display :0 -usepw -forever &
 socat TCP-LISTEN:$TWS_PORT,fork TCP:localhost:4001,forever &
 
 # Start this last and directly, so that if the gateway terminates for any reason, the container will stop as well.
